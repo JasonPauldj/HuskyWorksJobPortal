afd4510876a5ad8ed2572786d9a576921d7a8190
Jason Paul Darivemula <darivemula.j@northeastern.edu>
2022-04-07 10:35:37 -0400

    added student model

---
+++ server/api/models/student.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+//Student Schema
+const studentSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        lowercase : true,
+        trim : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    student_type :{
+        type : String,
+        required : true,
+        enum: ["GRAD","UNDER_GRAD"]
+    },
+    nuid :{
+        type : String,
+        required : true,
+        trim: true
+    },
+    gpa :{
+        type: Decimal128,
+        required : true
+    },
+    major : {
+        type: String,
+        required : true
+    },
+    email :{
+        type: String,
+        required : true,
+        match:  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    }
+});
+
+//Student Model
+const studentModel = mongoose.model("student", studentSchema);
+
+export default studentModel;
