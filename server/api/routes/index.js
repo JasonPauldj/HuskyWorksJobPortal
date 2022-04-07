d670cbbf7fde35e59d0e4450dce1200b734ffb7c
Jason Paul Darivemula <darivemula.j@northeastern.edu>
2022-04-07 10:57:58 -0400

    added student router

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,6 +1,8 @@
 import jobsRouter from "./jobs-router.js";
+import studentsRouter from "./students-router.js";
 
 // Exporting the routes using a default route
 export default (app) => {
   app.use("/", jobsRouter);
+  app.use("/", studentsRouter);
 };
