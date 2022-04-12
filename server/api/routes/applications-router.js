7d70ec40c6417ed504af9fa88a99e25d26d40e3c
eraricamehra <mehra.e@northeastern.edu>
2022-04-11 21:26:21 -0400

    Created Route for applicationa

---
+++ server/api/routes/applications-router.js
@@ -0,0 +1,11 @@
+import express from "express";
+// Imported controller methods to route urls to methods
+import * as ApplicationsController from "./../controllers/applications-controller.js";
+
+const router = express.Router();
+
+router
+  .route("/applications") // Entry Route
+  .post(ApplicationsController.createApplication) // Post Route
+ 
+export default router;
--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,6 +1,7 @@
 import jobsRouter from "./jobs-router.js";
 import studentsRouter from "./students-router.js";
 import eventsRouter from "./events-router.js";
+import applicationsRouter from "./applications-router.js";
 
 
 // Exporting the routes using a default route
@@ -8,4 +9,5 @@ export default (app) => {
   app.use("/", jobsRouter);
   app.use("/", studentsRouter);
   app.use("/", eventsRouter);
+  app.use("/", applicationsRouter);
 };
