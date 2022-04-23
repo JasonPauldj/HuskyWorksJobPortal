727ef21d55766dc0fb88f36f31aa8629fde4e738
Sutiksh Verma <verma.su@northeastern.edu>
2022-04-23 11:38:27 -0400

    Added Logout Functionality in navbar

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -11,27 +11,49 @@ import RecruiterSignUp from "./components/Login/RecruiterSignUp";
 import RecruiterDashboard from "./pages/dashboards/RecruiterDashboard";
 import StudentDashboard from "./pages/dashboards/StudentDashboard";
 import { useSelector } from "react-redux";
+import JobsPage from "./pages/jobs/JobsPage";
+import JobDetailPage from "./pages/jobs/JobDetailPage";
+import OrgDetailsPage from "./pages/organisation/OrgDetailsPage";
+import EventsPage from "./pages/events/EventsPage";
+import EventDetailsPage from "./pages/events/EventDetailsPage";
 
 function App() {
-
-  
-  const isAuth = useSelector(state => state.auth.isAuthenticated);
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   console.log(isAuth, "isAuth");
 
   return (
-    <div className="prbg"> 
-     
+    <div className="prbg">
       <Router>
         <div>
           <Routes>
-            <Route  path="/"  element={!isAuth ? <Login /> : <StudentProfile />}>
-            </Route>
+            <Route
+              path="/"
+              element={!isAuth ? <Login /> : <StudentProfile />}
+            ></Route>
             <Route path="/student/:id" element={<StudentProfile />}></Route>
             <Route path="/signup-student" element={<SignUpStudent />}></Route>
-            <Route path="/signup-recruiter" element={<RecruiterSignUp />}></Route>
-            <Route path="/dashboard-student/:id" element={<StudentDashboard />}></Route>
-            <Route path="/dashboard-recruiter/:id" element={<RecruiterDashboard />}></Route>
-            
+            <Route
+              path="/signup-recruiter"
+              element={<RecruiterSignUp />}
+            ></Route>
+            <Route
+              path="/dashboard-student/:id"
+              element={<StudentDashboard />}
+            ></Route>
+            <Route
+              path="/dashboard-recruiter/:id"
+              element={<RecruiterDashboard />}
+            ></Route>
+            <Route path="/jobs" element={<JobsPage />} />
+            <Route path="/jobs/:job_id" element={<JobDetailPage />} />
+
+            {/* Path for Organizations*/}
+            <Route path="/organizations" element={<OrgDetailsPage />} />
+            <Route path="/organizations/:org_id" element={<OrgDetailsPage />} />
+
+            {/* Path for Events */}
+            <Route path="/events" element={<EventsPage />} />
+            <Route path="/events/:event_id" element={<EventDetailsPage />} />
           </Routes>
         </div>
       </Router>
@@ -54,7 +76,6 @@ function App() {
         </div>
       </div> */}
     </div>
-
   );
 }
 
