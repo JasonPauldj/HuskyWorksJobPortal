f9cc6569af2d4488da6582fe1d6596c76dc492e5
Jason Paul Darivemula <darivemula.j@northeastern.edu>
2022-04-27 09:17:32 -0400

    expanded constants

--- webapp/src/utilities/constants.js
+++ webapp/src/utilities/constants.js
@@ -1,5 +1,5 @@
-export const JOB_CATEGORIES = ["SOFTWARE","HARDWARE", "AEROSPACE", "AUTOMOTIVE", "BIOMEDICAL"];
+export const JOB_CATEGORIES = ["SOFTWARE","HARDWARE", "AEROSPACE", "AUTOMOTIVE", "BIOMEDICAL", "CHEMICAL", "MECHANICAL", "ELECTRICAL", "NURSING", "ARTS", "PHARMACY", "DATA SCIENCE", "HEALTHCARE"];
 export const SKILLS = ["Java","Python"];
-export const JOB_LOCATIONS = ["Boston","New York","Los Angeles", "San Francisco","San Diego"];
+export const JOB_LOCATIONS = ["Boston","New York","Los Angeles", "San Francisco","San Diego", "Chicago", "Washington,D.C.","Miami", "New Orleans", "Orlando", "Nashville", "Seattle", "Austin", "Philadelphia", "Houston", "Atlanta", "Dallas"];
 export const JOB_TYPES = ["FULL-TIME", "PART-TIME", "INTERNSHIP"];
-export const PROGRAMS = ["MS Computer Science", "MS Information Systems", "MS Software Engineering Systems"];
+export const PROGRAMS = ["BA Computer Science","BS Business Administration","BS International Business","BS Information Science", "BS Chemical Engineering","MS Computer Science", "MS Information Systems", "MS Software Engineering Systems"];
